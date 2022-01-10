8af85877e2879c0e15eb343a8b3c2ca9ccaf13fa
kawakubox <shigefumi.kawakubo@gmail.com>
2022-01-10 13:48:27 +0900

    Log a SQSEvent

--- persist_message/main.go
+++ persist_message/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/rs/zerolog/log"
 	"net/http"
 )
 
 type Response events.APIGatewayProxyResponse
 
-func Handler(ctx context.Context) (Response, error) {
+func Handler(ctx context.Context, event events.SQSEvent) (Response, error) {
+	json, err := json.Marshal(event)
+	if err != nil {
+		log.Fatal().Err(err)
+		res := buildResponse(http.StatusInternalServerError, "Parsing SQS Event is failed.")
+		return res, err
+	}
+	log.Info().RawJSON("event", json).Msg("")
+
 	res := buildResponse(http.StatusOK, "OK")
 	return res, nil
 }
